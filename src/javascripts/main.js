2eda4bb99969cb8a6f86721f18e3664aa6dc1c99
Austin Moore <austinmoore3@mail.weber.edu>
2023-11-04 03:28:18 -0600

    Changed the nav bar - styling

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,9 +2,9 @@
 import "bootstrap"
 
 document.getElementById("navbar").innerHTML = `
-<nav class="navbar navbar-expand-lg webercolor navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/pages/index.html"><i class="bi bi-cpu"></i> STA Spot</a>
+<nav class="navbar navbar-expand-lg webercolor navbar-dark py-1">
+        <div class="container-fluid align-items-baseline">
+            <a class="navbar-brand" href="/pages/index.html"><i class="bi bi-cpu fs-1 me-2"></i><h2 class="d-inline-flex bg-white webertext p-1 rounded-3 me-1">STA</h2> Spot</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -12,7 +12,7 @@ document.getElementById("navbar").innerHTML = `
             <div class="navbar-nav">
               <a class="nav-link" aria-current="page" href="/pages/index.html">Home</a>
               <a class="nav-link" href="/pages/maps&ous.html">Maps & OUs</a>
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="/pages/imaging.html">Imaging</a>
               <a class="nav-link disabled">Disabled</a>
             </div>
           </div>
